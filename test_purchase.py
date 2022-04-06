9bd997f71af2acb1b566c31f6ee9d287a844bb08
Gaurav-00 <shindegaurav5551@gmail.com>
2022-04-06 11:41:21 +0530

    setup started

--- test_purchase.py
+++ test_purchase.py
@@ -1,8 +1,12 @@
 import pytest
 
-def test_AddItemtoCart():
+@pytest.fixture()
+def setUp():
+    print("Setup started")
+
+def test_AddItemtoCart(setUp):
     print("Add item successfully")
 
-def test_RemoveItemfromCart():
+def test_RemoveItemfromCart(setUp):
     print("Removed item successfully")
 
