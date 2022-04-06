b4aa7f7fad5f42b01fd191b9a4e10628ddbed19c
Gaurav-00 <shindegaurav5551@gmail.com>
2022-04-06 11:44:31 +0530

    setup started

--- test_purchase.py
+++ test_purchase.py
@@ -1,8 +1,14 @@
 import pytest
 
+#teardown fiunction working in pytest
+
+
 @pytest.fixture()
 def setUp():
     print("Setup started")
+    # teardown fiunction working in pytest
+    yield
+    print("exited")
 
 def test_AddItemtoCart(setUp):
     print("Add item successfully")
